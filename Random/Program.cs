919f0eb1e9d4ea04dbabbc57e563fa09c0917d6a
HaGaDDD <bozinandrey1@yandex.ru>
2021-10-28 15:31:12 +0300

    Add project files.

---
+++ Random/Program.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using System;
+using System.IO;
+
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faker f = new Faker("ru");
+            var rnd = new Randomizer();
+            string writePath = @"C:\Users\Андрей\Desktop\testData.txt"; //  заменить на ваш путь
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    for (int i = 1; i < 10; ++i)    //  индекс
+                    {
+                        var fn = f.Name.FirstName();
+                        var ln = f.Name.LastName();
+                        var exp = rnd.Int(0, 30);
+                        sw.WriteLine($"{i}\t{fn}\t{ln}\t{exp}");    // \t нужны для копипаста
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
